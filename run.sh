577306b36b8f31d3b1ab01e0807a6ecdad5f6d14
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2013-11-06 19:27:46 -0500

    Mavening project

--- run.sh
+++ run.sh
@@ -4,21 +4,19 @@
 
 # remove build artifacts
 function clean() {
-    rm -rf obj log
+    mvn clean
+    rm -rf log
 }
 
-# compile the source code for procedures and the client
-function compile() {
-    mkdir -p obj
-    javac -classpath $CLASSPATH -d obj -Xlint:deprecation \
-        src/*.java
-    # stop if compilation fails
-    if [ $? != 0 ]; then exit; fi
+# assemble the source code for procedures and the client
+function assemble() {
+    mvn assembly:assembly # override db.groupid, db.artifactid and db.version in pom.xml before proceeding
 }
 
 function loader() {
-    compile
-    java -Xmx512m -Dfile.encoding=UTF-8 -classpath obj:$CLASSPATH \
+    if [ ! -d ./target ]; then assemble; fi
+
+    java -Xmx512m -Dfile.encoding=UTF-8 -classpath target/*:$CLASSPATH \
         -Dlog4j.configuration=file://$LOG4J \
         jdbcloader.JdbcLoader \
         --jdbcurl="jdbc:mysql://localhost/test?useLegacyDatetimeCode=false&serverTimezone=America/New_York&characterEncoding=UTF-8&useUnicode=true" \
@@ -31,7 +29,7 @@ function loader() {
 }
 
 function help() {
-    echo "Usage: ./run.sh {help|clean|loader|compile}"
+    echo "Usage: ./run.sh {help|clean|loader|assemble}"
 }
 
 # Run the target passed as the first arg on the command line
